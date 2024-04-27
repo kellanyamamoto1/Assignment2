7b02fe597c7fffbe1923c1883003673118aad5db
kellany <kellany@uci.edu>
2024-04-26 20:12:28 -0700

    name written and files downloaded

---
+++ a2.py
@@ -0,0 +1,10 @@
+# a2.py
+
+# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Kellan Yamamoto
+# kellany@uci.edu
+# 28388886
+
