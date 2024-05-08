ef27d4dc70d2b34cb1eb5da0b3a848b6e3685421
kellany <kellany@uci.edu>
2024-05-08 16:58:27 -0700

    commands list addded

--- user.py
+++ user.py
@@ -8,6 +8,17 @@ from pathlib import Path
 import admin as admin
 import ui as ui
 
+command_list = """
+L -- List all files in a specified path
+Q -- Quit the program entirely
+C -- Create a new file in a specified path
+D -- Deleted a file
+R -- Read a file
+O -- Open a file
+Help -- Relist commands
+"""
+
+
 def start():
     ui.admin(0)
     ui.commands()
